aa03966762401140c931bd6c1213f9be74320990
ragnoros <akli.reguig@gmail.com>
2013-09-23 01:45:37 +0200

    i

---
+++ src/com/ragnoros/moneyio/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ragnoros.moneyio;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
